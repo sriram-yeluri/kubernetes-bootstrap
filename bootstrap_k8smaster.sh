96fe9343c1370a5825360cb3e80266b0084d6178
Sriram Yeluri <Mac@Apples-Mac-mini.home>
2022-01-31 16:23:45 +0100

    fixed setting config for vagrant user

--- bootstrap_k8smaster.sh
+++ bootstrap_k8smaster.sh
@@ -28,6 +28,11 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# set config for vagrant user
+mkdir -p /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
+
 # Install calico pod network
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
